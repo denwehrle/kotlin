44b9173c6ab4c4b5a368803990403a3cfe2c094a
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2019-03-07 13:08:14 +0300

    -- allow to throw exceptions when version is patched

--- idea/src/org/jetbrains/kotlin/idea/actions/internal/KotlinThrowExceptionAction.kt
+++ idea/src/org/jetbrains/kotlin/idea/actions/internal/KotlinThrowExceptionAction.kt
@@ -7,8 +7,17 @@ package org.jetbrains.kotlin.idea.actions.internal
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import org.jetbrains.kotlin.idea.KotlinPluginUtil
 
 class KotlinThrowExceptionAction : AnAction() {
+    override fun update(e: AnActionEvent?) {
+        super.update(e)
+
+        if (e != null && KotlinPluginUtil.isPatched()) {
+            e.presentation.isEnabledAndVisible = true
+        }
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
         throw IllegalStateException("Kotlin Test Exception")
     }
